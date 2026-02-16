f3a4631cc246d72de493d307225a9198bf4cfa69
NafizImrose <nafizimrose.official@gmail.com>
2026-02-17 03:34:19 +0600

    add interactivity to demo section with background color toggle

--- script.js
+++ script.js
@@ -16,3 +16,38 @@ document.getElementById("btn").addEventListener("click", function () {
     toggled = true;
   }
 });
+
+// let flag = true;
+// document.getElementById("demo").addEventListener("click", function (event) {
+//   if (
+//     event.target.parentNode.id === "demo" ||
+//     event.target.id === "demo" ||
+//     event.target.id === "image"
+//   ) {
+//     if (flag) {
+//       this.style.background = "linear-gradient(to bottom, #68f7f7, #defafa)";
+//       flag = false;
+//     } else {
+//       this.style.background = "linear-gradient(to bottom, #F4F4F4, #FFFFFF)";
+//       flag = true;
+//     }
+//   }
+// });
+
+const demo = document.getElementById("demo");
+let flag = true;
+document.addEventListener("click", function (event) {
+  if (demo.contains(event.target)) {
+    // Clicked inside #demo
+    if (flag) {
+      demo.style.background = "linear-gradient(to bottom, #68f7f7, #defafa)";
+      flag = false;
+    } else {
+      demo.style.background = "linear-gradient(to bottom, #F4F4F4, #FFFFFF)";
+      flag = true;
+    }
+  } else {
+    // Clicked outside #demo
+    demo.style.background = "linear-gradient(to bottom, #F4F4F4, #FFFFFF)";
+  }
+});
